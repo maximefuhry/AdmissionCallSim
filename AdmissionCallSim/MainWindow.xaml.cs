92078b56a9eae51cc0d90d97bc93454592cced02
maximefuhry <maxime.fuhry@gmail.com>
2015-04-22 15:49:14 +0200

    réalisation d'un canvas + drag image. Reste a ajouter des téléphones et interdire la sortie du canvas à droite
    Signed-off-by: maximefuhry <maxime.fuhry@gmail.com>

--- AdmissionCallSim/MainWindow.xaml.cs
+++ AdmissionCallSim/MainWindow.xaml.cs
@@ -20,9 +20,54 @@ namespace AdmissionCallSim
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image draggedImage;
+        private Point mousePosition;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var image = e.Source as Image;
+
+            if (image != null && canvas.CaptureMouse())
+            {
+                mousePosition = e.GetPosition(canvas);
+                draggedImage = image;
+                Panel.SetZIndex(draggedImage, 1); // in case of multiple images
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                canvas.ReleaseMouseCapture();
+                Panel.SetZIndex(draggedImage, 0);
+                draggedImage = null;
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                var position = e.GetPosition(canvas);
+                var offset = position - mousePosition;
+                mousePosition = position;
+
+                if (Canvas.GetLeft(draggedImage) + offset.X > 0)
+                {
+                    Canvas.SetLeft(draggedImage, Canvas.GetLeft(draggedImage) + offset.X);
+                }
+                if (Canvas.GetTop(draggedImage) + offset.Y + draggedImage.Height < canvas.Height && Canvas.GetTop(draggedImage) + offset.Y > 0)
+                {
+                    Canvas.SetTop(draggedImage, Canvas.GetTop(draggedImage) + offset.Y);
+                }
+            }
+        }
+
     }
 }
